2bb658678da9ac9a03ae4863b67116f66af6e3c5
root <davidgamboajunior@gmail.com>
2021-06-30 00:27:19 +0000

    primer commit

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,11 +66,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '192.168.10.11'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'prueba'),
+            'username' => env('DB_USERNAME', 'prueba'),
+            'password' => env('DB_PASSWORD', 'prueba'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
